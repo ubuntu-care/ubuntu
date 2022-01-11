9c6f7fa83a491eac1601fd6882acc7525da95595
ngaruko <bede.ngaruko@gmail.com>
2022-01-12 07:25:52 +1300

    branding and copyright changes

--- frontend/src/app/@core/interfaces/common/settings.ts
+++ frontend/src/app/@core/interfaces/common/settings.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Copyright (c) Ubuntu Care 2022. All Rights Reserved.
  * Licensed under the Single Application / Multi Application License.
  * See LICENSE_SINGLE_APP / LICENSE_MULTI_APP in the 'docs' folder for license information on type of purchased license.
  */
@@ -7,7 +7,7 @@
 import { Observable } from 'rxjs';
 
 /*
- * Copyright (c) Akveo 2019. All Rights Reserved.
+ * Copyright (c) Ubuntu Care 2022. All Rights Reserved.
  * Licensed under the Personal / Commercial License.
  * See LICENSE_PERSONAL / LICENSE_COMMERCIAL in the project root for license information on type of purchased license.
  */
